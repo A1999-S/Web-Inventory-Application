13d0eebaa642ab9ef3bfbd72e5ab95eddaee789d
ashai39 <ashai39@wgu.edu>
2024-03-11 21:35:06 -0700

    Updated inventory to include 5 parts and 5 products.

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,5 +1,6 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
@@ -40,7 +41,8 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // Checking to see if parts already exist
+
+/*        // Checking to see if parts already exist
         if (partRepository.count() <= 0) {
 
             // Part 1, Soft Tire
@@ -50,25 +52,38 @@ public class BootStrapData implements CommandLineRunner {
             o.setInv(1);
             o.setPrice(675.0);
             o.setId(100);
+            o.setInvMin(0);
+            o.setInvMax(4);
             outsourcedPartRepository.save(o);
+*//*            List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for(OutsourcedPart part:outsourcedParts){
+                if(part.getName().equals("out test"))thePart=part;
+            }*//*
 
 
-            // Part 2, SuperSoft Tire
+
+        // Part 2, SuperSoft Tire
             OutsourcedPart o1 = new OutsourcedPart();
             o1.setCompanyName("Aston Martin F1");
             o1.setName("SuperSoft Tire");
             o1.setInv(1);
             o1.setPrice(675.0);
             o1.setId(101);
+            o1.setInvMin(0);
+            o1.setInvMax(4);
             outsourcedPartRepository.save(o1);
 
-            // Part 3, Medium Tire
+
+
+        // Part 3, Medium Tire
             OutsourcedPart o2 = new OutsourcedPart();
             o2.setCompanyName("Aston Martin F1");
             o2.setName("Medium Tire");
             o2.setInv(1);
             o2.setPrice(675.0);
             o2.setId(102);
+            o2.setInvMin(0);
+            o2.setInvMax(4);
             outsourcedPartRepository.save(o2);
 
             // Part 4, Hard Tire
@@ -78,6 +93,8 @@ public class BootStrapData implements CommandLineRunner {
             o3.setInv(1);
             o3.setPrice(675.0);
             o3.setId(103);
+            o3.setInvMin(0);
+            o3.setInvMax(4);
             outsourcedPartRepository.save(o3);
 
             // Part 5, Wet Tire
@@ -87,9 +104,16 @@ public class BootStrapData implements CommandLineRunner {
             o4.setInv(1);
             o4.setPrice(675.0);
             o4.setId(104);
+            o4.setInvMin(0);
+            o4.setInvMax(4);
             outsourcedPartRepository.save(o4);
+            OutsourcedPart thePart=null;
+            List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for(OutsourcedPart part:outsourcedParts){
+                if(part.getName().equals("out test"))thePart=part;
+            }
 
-        }
+        }*/
 
 
 /*        System.out.println(thePart.getCompanyName());
@@ -98,7 +122,7 @@ public class BootStrapData implements CommandLineRunner {
             System.out.println(part.getName()+" "+part.getCompanyName());
         }*/
 
-        // Checking to see if products already exist
+ /*       // Checking to see if products already exist
         if (productRepository.count() <= 0) {
 
             Product carSoft = new Product("AMR24 F1 Race Car (with Soft Tires)", 15000000, 1);
@@ -114,13 +138,14 @@ public class BootStrapData implements CommandLineRunner {
             productRepository.save(carWet);
 
 
-/*            System.out.println("Started in Bootstrap");
+
+*//*            System.out.println("Started in Bootstrap");
             System.out.println("Number of Products" + productRepository.count());
             System.out.println(productRepository.findAll());
             System.out.println("Number of Parts" + partRepository.count());
             System.out.println(partRepository.findAll());
-            */
+            *//*
 
-        }
+        }*/
     }
 }
