466d1b58482eb389d78bce97a362fd6d832acb49
ashai39 <ashai39@wgu.edu>
2024-03-11 21:39:54 -0700

    Added min and max inventory fields to part forms. Added @Min and @Max to ensure inventory is within constraints Added min and max inventory section to inventory.

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -2,7 +2,9 @@ package com.example.demo.domain;
 
 import com.example.demo.validators.ValidDeletePart;
 
+
 import javax.persistence.*;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -16,6 +18,7 @@ import java.util.Set;
  */
 @Entity
 @ValidDeletePart
+
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="part_type",discriminatorType = DiscriminatorType.INTEGER)
 @Table(name="Parts")
@@ -28,6 +31,14 @@ public abstract class Part implements Serializable {
     double price;
     @Min(value = 0, message = "Inventory value must be positive")
     int inv;
+    @Min(value = 0, message = "Inventory value must be positive")
+    @Max(value = 6, message = "Inventory cannot exceed 5")
+    int invMin;
+
+    @Min(value = 0, message = "Inventory value must be positive")
+    @Max(value = 6, message = "Inventory cannot exceed 5")
+    int invMax;
+
 
     @ManyToMany
     @JoinTable(name="product_part", joinColumns = @JoinColumn(name="part_id"),
@@ -37,17 +48,21 @@ public abstract class Part implements Serializable {
     public Part() {
     }
 
-    public Part(String name, double price, int inv) {
+    public Part(String name, double price, int inv, int invMin, int invMax) {
         this.name = name;
         this.price = price;
         this.inv = inv;
+        this.invMin = invMin;
+        this.invMax = invMax;
     }
 
-    public Part(long id, String name, double price, int inv) {
+    public Part(long id, String name, double price, int inv, int invMin, int invMax) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.inv = inv;
+        this.invMin = invMin;
+        this.invMax = invMax;
     }
 
     public long getId() {
@@ -80,6 +95,24 @@ public abstract class Part implements Serializable {
 
     public void setInv(int inv) {
         this.inv = inv;
+
+    }
+    public int getInvMin() {
+        return invMin;
+    }
+
+    public void setInvMin(int invMin) {
+        this.invMin = invMin;
+
+    }
+
+    public int getInvMax() {
+        return invMax;
+    }
+
+    public void setInvMax(int invMax) {
+        this.invMax = invMax;
+
     }
 
     public Set<Product> getProducts() {
@@ -107,4 +140,37 @@ public abstract class Part implements Serializable {
     public int hashCode() {
         return (int) (id ^ (id >>> 32));
     }
+
+    // Checks if inventory is within constraints
+    public boolean validInventory(int inv, int invMin, int invMax) {
+        if (inv >= invMin && inv <= invMax) {
+            return true;
+        }
+        return false;
+    }
 }
+/*
+public void setInv(int inv) {
+    if (inv >= invMin && inv <= invMax) {
+        this.inv = inv;
+    }
+}
+public int getInvMin() {
+    return invMin;
+}
+
+public void setInvMin(int invMin) {
+    if (invMin >= 0 && invMin <= invMax) {
+        this.invMin = invMin;
+    }
+}
+
+public int getInvMax() {
+    return invMax;
+}
+
+public void setInvMax(int invMax) {
+    if (invMax >= invMin) {
+        this.invMax = invMax;
+    }
+}*/
